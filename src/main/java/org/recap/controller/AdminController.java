db88d45a474d75bc0f0d54f5471a571afcd15454
DinakarCiber <dnaramala@ciber.com>
2020-11-24 17:32:40 +0530

    changes in admin controller

--- src/main/java/org/recap/controller/AdminController.java
+++ src/main/java/org/recap/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
 /**
  * Created by dinakar on 24/11/20.
  */
@@ -11,20 +12,15 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin
 @RequestMapping("/admin")
 public class AdminController {
-    private static final Logger logger = LoggerFactory.getLogger(BulkRequestController.class);
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
-    @PostMapping(value = "/upload")
+    @PostMapping("/upload")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
         try {
             String content = new String(file.getBytes());
-            logger.info("xml Data -->" + content);
+            logger.info("xml file data ::" + content);
             message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            if (!file.isEmpty()) {
-                return "file empty";
-            } else {
-                return "file not empty";
-            }
         } catch (Exception e) {
             message = "Could not upload the file: " + file.getOriginalFilename() + "!";
         }
