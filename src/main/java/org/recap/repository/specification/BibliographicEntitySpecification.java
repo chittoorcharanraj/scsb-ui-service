39f090dbe2eaffde765903d3fbc5e0f98f3d3f86
reachram <reachramanujam79@gmail.com>
2021-01-17 13:28:25 -0500

    Added specification for Bibliographic.

---
+++ src/main/java/org/recap/repository/specification/BibliographicEntitySpecification.java
@@ -0,0 +1,7 @@
+package org.recap.repository.specification;
+
+import org.recap.model.jpa.BibliographicEntity;
+import org.recap.repository.BaseSpecification;
+
+public class BibliographicEntitySpecification extends BaseSpecification<BibliographicEntity> {
+}
