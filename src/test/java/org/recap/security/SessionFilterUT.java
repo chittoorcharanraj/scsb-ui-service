2dc286036cc4d88d3f5905035a325fbcec598486
aniitavenkatesan <anitha.venkatesan@htcindia.com>
2021-02-25 14:13:28 +0530

    Junit Changes

--- src/test/java/org/recap/security/SessionFilterUT.java
+++ src/test/java/org/recap/security/SessionFilterUT.java
@@ -1,28 +1,45 @@
 package org.recap.security;
 
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.recap.BaseTestCaseUT;
+import org.recap.RecapConstants;
 import org.recap.spring.ApplicationContextProvider;
+import org.recap.util.HelperUtil;
 import org.recap.util.UserAuthUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
-public class SessionFilterUT extends BaseTestCaseUT {
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({HelperUtil.class,SecurityContextHolder.class})
+@PowerMockIgnore("javax.security.*")
+public class SessionFilterUT extends BaseTestCaseUT{
     @InjectMocks
     SessionFilter sessionFilter;
     @Mock
-    ServletRequest request;
+    HttpServletRequest request;
     @Mock
-    ServletResponse response;
+    HttpServletResponse response;
     @Mock
     FilterChain filterChain;
     @Mock
@@ -33,30 +50,43 @@ public class SessionFilterUT extends BaseTestCaseUT {
     UserAuthUtil userAuthUtil;
 
     @Mock
+    HttpSession httpSession;
+
+    @Mock
+    UserInstitutionCache userInstitutionCache;
+
+    @Mock
+    UsernamePasswordToken usernamePasswordToken;
+
+    @Mock
+    FilterConfig filterConfig;
+
+    @Mock
     ApplicationContextProvider applicationContextProvider;
 
     @Test
     public void doFilter() throws Exception {
-       // Mockito.when(context.getAuthentication()).thenReturn(authentication);
-//        Mockito.doCallRealMethod().when(sessionFilter).doFilter(request, response, filterChain);
+        Mockito.when(context.getAuthentication()).thenReturn(authentication);
+        PowerMockito.mockStatic(HelperUtil.class);
+        PowerMockito.mockStatic(SecurityContextHolder.class);
+        when(HelperUtil.isAnonymousUser(Mockito.any())).thenReturn(false);
+        when(HelperUtil.getBean(Mockito.any())).thenReturn(userInstitutionCache);
+        Mockito.when(userInstitutionCache.getInstitutionForRequestSessionId(Mockito.anyString())).thenReturn("PUL");
+        when(SecurityContextHolder.getContext()).thenReturn(context);
+        Mockito.when(request.getSession()).thenReturn(httpSession);
+        Mockito.when(httpSession.getId()).thenReturn("1");
+        Mockito.when(request.getSession(false)).thenReturn(httpSession);
+        Mockito.when(httpSession.getAttribute(RecapConstants.USER_TOKEN)).thenReturn(usernamePasswordToken);
+        Mockito.when(userAuthUtil.authorizedUser(RecapConstants.SCSB_SHIRO_TOUCH_EXISTIN_SESSION_URL, usernamePasswordToken)).thenReturn(true);
         sessionFilter.doFilter(request, response, filterChain);
+        assertTrue(true);
     }
 
     @Test
-    public void destroy() {
+    public void testDoNothingMethods() throws ServletException {
         sessionFilter.destroy();
+        sessionFilter.init(filterConfig);
+        assertTrue(true);
     }
-    @Test
-    public void getUserAuthUtil() {
-        UserAuthUtil userAuthUtil = sessionFilter.getUserAuthUtil();
-        assertNotNull(userAuthUtil);
-    }
-    @Test
-    public void getUserAuthUtilNewUserAuthUtil() {
-        SessionFilter sessionFilter = new SessionFilter();
-        try {
-           sessionFilter.getUserAuthUtil();
-        }catch (Exception e){}
 
-    }
 }
--- src/test/java/org/recap/util/UserAuthUtilUT.java
+++ src/test/java/org/recap/util/UserAuthUtilUT.java
@@ -2,23 +2,20 @@ package org.recap.util;
 
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.junit.Test;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.recap.BaseTestCase;
 import org.recap.BaseTestCaseUT;
 import org.recap.RecapConstants;
-import org.recap.model.usermanagement.UserForm;
+import org.recap.model.usermanagement.UserDetailsForm;
 import org.recap.service.RestHeaderService;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doReturn;
 
 /**
  * Created by dharmendrag on 7/2/17.
@@ -34,24 +31,40 @@ public class UserAuthUtilUT extends BaseTestCaseUT {
     @Mock
     RestTemplate restTemplate;
 
+    @Mock
+    UsernamePasswordToken token;
+
+    @Mock
+    HttpSession session;
+
+    @Mock
+    HttpServletRequest httpServletRequest;
+
+    @Mock
+    HttpSession httpSession;
+
+
     @Test
-    public void testLoginAndAuthorization()throws Exception{
-        boolean authorized=false;
-        UserForm userForm=new UserForm();
-        userForm.setUsername("john");
-        userForm.setInstitution("CUL");
-        userForm.setPassword("123");
-
-        Map<String,Object> map = new HashMap<>();
-        map.put("isAuthenticated",true);
-        map.put("userName","john");
-        map.put(RecapConstants.USER_ID,"CUL");
-        UsernamePasswordToken token=new UsernamePasswordToken(userForm.getUsername()+ RecapConstants.TOKEN_SPLITER +"CUL",userForm.getPassword(),true);
-        try {
-            Map<String, Object> resultmap = (Map<String, Object>) userAuthUtil.doAuthentication(token);
-        }catch (Exception e){}
-        authorized=userAuthUtil.authorizedUser(RecapConstants.SCSB_SHIRO_COLLECTION_URL,token);
-
+    public void testdoAuthentication() {
+        Mockito.when(session.getAttribute(Mockito.anyString())).thenReturn(true);
+        Mockito.when(session.getAttribute(RecapConstants.USER_INSTITUTION)).thenReturn(1);
+        UserDetailsForm userDetailsForm=userAuthUtil.getUserDetails(session,"");
+        assertTrue(userDetailsForm.isRecapUser());
     }
+
+    @Test
+    public void testLoginAndAuthorizationExceptionLogOut() {
+        boolean authorized = userAuthUtil.authorizedUser(RecapConstants.LOGOUT, token);
+        assertFalse(authorized);
+    }
+
+    @Test
+    public void testLoginAndAuthorException() {
+        Mockito.when(httpServletRequest.getSession(false)).thenReturn(httpSession);
+        Mockito.when(httpSession.getAttribute(RecapConstants.USER_TOKEN)).thenReturn(token);
+        boolean authorized = userAuthUtil.isAuthenticated(httpServletRequest,RecapConstants.SCSB_SHIRO_COLLECTION_URL);
+        assertFalse(authorized);
+    }
+
 
 }
