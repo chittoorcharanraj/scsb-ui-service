89821dac1a2457295a87f8261dc29caf50c9497b
DinakarCiber <dnaramala@ciber.com>
2021-04-27 11:23:49 +0530

    changes for storage Location

--- src/main/java/org/recap/model/jpa/BulkRequestItemEntity.java
+++ src/main/java/org/recap/model/jpa/BulkRequestItemEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.AttributeOverride;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.JoinTable;
@@ -28,4 +29,13 @@ public class BulkRequestItemEntity extends BulkRequestItemAbstractEntity {
             joinColumns = @JoinColumn(name = "BULK_REQUEST_ID"),
             inverseJoinColumns = @JoinColumn(name = "REQUEST_ID"))
     private List<RequestItemEntity> requestItemEntities;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "IMS_LOCATION_ID", referencedColumnName = "IMS_LOCATION_ID", insertable = false, updatable = false)
+    private ImsLocationEntity imsLocationEntity;
+
+    @Column(
+            name = "IMS_LOCATION_ID"
+    )
+    private Integer imsLocationCode;
 }
--- src/main/java/org/recap/model/search/BulkSearchResultRow.java
+++ src/main/java/org/recap/model/search/BulkSearchResultRow.java
@@ -23,5 +23,5 @@ public class BulkSearchResultRow {
     private Date createdDate;
     private String status;
     private String bulkRequestNotes;
-
+    private String imsLocation;
 }
