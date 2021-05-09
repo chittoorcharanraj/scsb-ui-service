dcc6b2ecdc636caf7f10b0c6ac1788e4be1b7949
DinakarCiber <dnaramala@ciber.com>
2021-05-09 19:31:52 +0530

    changes for refactor

--- src/main/java/org/recap/controller/HomeController.java
+++ src/main/java/org/recap/controller/HomeController.java
@@ -88,7 +88,6 @@ public class HomeController extends AbstractController {
             resultMap.put(ScsbConstants.USER_NAME, request.getSession().getAttribute(ScsbConstants.USER_NAME));
             resultMap.put(ScsbConstants.USER_DESC, request.getSession().getAttribute(ScsbConstants.USER_DESC));
             resultMap.put(ScsbConstants.REQUEST_PRIVILEGE, request.getSession().getAttribute(ScsbConstants.REQUEST_PRIVILEGE));
-            resultMap.put(ScsbConstants.REQUEST_PRIVILEGE, request.getSession().getAttribute(ScsbConstants.REQUEST_PRIVILEGE));
             resultMap.put(ScsbConstants.SEARCH_PRIVILEGE, request.getSession().getAttribute(ScsbConstants.SEARCH_PRIVILEGE));
             resultMap.put(ScsbConstants.COLLECTION_PRIVILEGE, request.getSession().getAttribute(ScsbConstants.COLLECTION_PRIVILEGE));
             resultMap.put(ScsbConstants.DEACCESSION_PRIVILEGE, request.getSession().getAttribute(ScsbConstants.DEACCESSION_PRIVILEGE));
