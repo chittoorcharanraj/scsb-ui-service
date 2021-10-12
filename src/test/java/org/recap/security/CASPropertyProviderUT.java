5a0b1e62ba978b921d94c908910c6dd64a392b0c
JancyRoachR <jancy.roach@htcindia.com>
2021-10-12 11:22:18 +0530

    UT changes Stage4##1

--- src/test/java/org/recap/security/CASPropertyProviderUT.java
+++ src/test/java/org/recap/security/CASPropertyProviderUT.java
@@ -1,27 +1,46 @@
 package org.recap.security;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.recap.BaseTestCase;
+import org.recap.BaseTestCaseUT;
 import org.recap.PropertyKeyConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.cas.ServiceProperties;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.authentication.CasAuthenticationToken;
+import org.springframework.security.cas.authentication.StatelessTicketCache;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * Created by hemalathas on 30/3/17.
  */
-public class CASPropertyProviderUT extends BaseTestCase {
+public class CASPropertyProviderUT extends BaseTestCaseUT {
 
-    @Autowired
+    @InjectMocks
     CASPropertyProvider casPropertyProvider;
 
-    @Value("${" + PropertyKeyConstants.SCSB_APP_SERVICE_SECURITY + "}")
-    private String security;
 
-    @Value("${" + PropertyKeyConstants.SCSB_APP_SERVICE_HOME + "}")
-    private String home;
+    private String security = "test";
+
+    private String home = "test";
+
+    @Before
+    public void setup(){
+        ReflectionTestUtils.setField(casPropertyProvider,"security",security);
+        ReflectionTestUtils.setField(casPropertyProvider,"home",home);
+    }
 
     @Test
     public void testCASPropertyProvider(){
@@ -31,5 +50,4 @@ public class CASPropertyProviderUT extends BaseTestCase {
         assertNotNull(casPropertyProvider.getHome());
         assertNotNull(casPropertyProvider.getSecurity());
     }
-
 }
