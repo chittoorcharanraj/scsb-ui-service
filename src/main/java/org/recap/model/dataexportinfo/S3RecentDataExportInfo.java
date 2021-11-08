81f1768c5c317a697335546d1f38b6edf4cff381
ndinakar <dinakarn31@gmail.com>
2021-11-08 08:21:24 +0530

    changes for scsb-739

--- src/main/java/org/recap/model/dataexportinfo/S3RecentDataExportInfo.java
+++ src/main/java/org/recap/model/dataexportinfo/S3RecentDataExportInfo.java
@@ -1,7 +1,13 @@
 package org.recap.model.dataexportinfo;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class S3RecentDataExportInfo {
 
     private String institution;
@@ -12,68 +18,4 @@ public class S3RecentDataExportInfo {
     private String itemCount;
     private long keySize;
     private Date keyLastModified;
-
-    public String getInstitution() {
-        return institution;
-    }
-
-    public void setInstitution(String institution) {
-        this.institution = institution;
-    }
-
-    public String getBibDataFormat() {
-        return bibDataFormat;
-    }
-
-    public void setBibDataFormat(String bibDataFormat) {
-        this.bibDataFormat = bibDataFormat;
-    }
-
-    public String getKeyName() {
-        return keyName;
-    }
-
-    public void setKeyName(String keyName) {
-        this.keyName = keyName;
-    }
-
-    public String getGcd() {
-        return gcd;
-    }
-
-    public void setGcd(String gcd) {
-        this.gcd = gcd;
-    }
-
-    public String getBibCount() {
-        return bibCount;
-    }
-
-    public void setBibCount(String bibCount) {
-        this.bibCount = bibCount;
-    }
-
-    public String getItemCount() {
-        return itemCount;
-    }
-
-    public void setItemCount(String itemCount) {
-        this.itemCount = itemCount;
-    }
-
-    public long getKeySize() {
-        return keySize;
-    }
-
-    public void setKeySize(long keySize) {
-        this.keySize = keySize;
-    }
-
-    public Date getKeyLastModified() {
-        return keyLastModified;
-    }
-
-    public void setKeyLastModified(Date keyLastModified) {
-        this.keyLastModified = keyLastModified;
-    }
 }
