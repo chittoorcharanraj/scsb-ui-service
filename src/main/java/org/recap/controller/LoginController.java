d9f78fa985a5742ae85f1f121ed571004f6e027b
chittoorcharanraj <chittoor.charanraj@htcinc.com>
2025-09-05 12:22:49 +0530

    remove unused import

--- src/main/java/org/recap/controller/LoginController.java
+++ src/main/java/org/recap/controller/LoginController.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
